e6524f6b789ca85460aecb425054ac354e69ae25
Christoph Bonitz <ml.christophbonitz@gmail.com>
2019-01-28 00:31:21 +0100

    antialiasing

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 extern crate image;
 extern crate rand;
-
 extern crate nalgebra as na;
+extern crate rayon;
+
 use na::Vector3;
 #[allow(unused_imports)]
 use rand::{thread_rng, Rng};
 use std::f32;
+use rayon::prelude::*;
 
 const MAX_DEPTH: u32 = 5;
+const ANTIALIASING: usize = 100;
 
 pub enum Surface {
     Diffuse { color: Vector3<f32>},
@@ -164,23 +167,30 @@ fn render(
     let delta_y = 2.0 * inverse_origin / dimensions.1 as f32;
     let upper_left = (-1.0, inverse_origin);
     let number_of_pixels = dimensions.0 * dimensions.1;
-    let mut pixels = vec![Vector3::<f32>::new(0.0, 0.0, 0.0); number_of_pixels];
-    for (i, pixel) in pixels.iter_mut().enumerate() {
+    let indices: Vec<usize> = (0..number_of_pixels).collect();
+    let antialiasing_scale_factor = 1.0 / ANTIALIASING as f32;
+    indices.par_iter().map(|i| -> Vector3<f32> {
+        let mut pixel = Vector3::<f32>::new(0.0, 0.0, 0.0);
         let offset_right = (i % dimensions.0) as f32 * delta_x;
         let offset_down = (i / dimensions.0) as f32 * delta_y;
-
-        let ray = Ray::from_to(
-            origin,
-            &Vector3::<f32>::new(
-                upper_left.0 + offset_right,
-                upper_left.1 - offset_down,
-                film_distance,
-            ),
-        );
-
-        *pixel = trace_ray(&ray, spheres, lights, 1);
-    }
-    pixels
+        let mut rng = rand::thread_rng();
+        let pixel_origin_x = upper_left.0 + offset_right;
+        let pixel_origin_y = upper_left.1 - offset_down;
+        for _ in 0..ANTIALIASING {
+            let random_x: f32 = rng.gen();
+            let random_y: f32 = rng.gen();
+            let ray = Ray::from_to(
+                origin,
+                &Vector3::<f32>::new(
+                     pixel_origin_x + random_x * delta_x,
+                     pixel_origin_y - random_y * delta_y,
+                    film_distance,
+                ),
+            );
+            pixel += trace_ray(&ray, spheres, lights, 1);
+        }
+        pixel * antialiasing_scale_factor
+    }).collect()
 }
 
 fn expose(pixels: &Vec<Vector3<f32>>) -> Vec<Vector3<f32>> {
